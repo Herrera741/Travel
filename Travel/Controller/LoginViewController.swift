3946ba695270e6b842716955a14e85d8fdf74e35
Sergio <sherrera725@gmail.com>
2023-02-20 00:34:54 -0800

    add LoginViewController and init UI elements

---
+++ Travel/Controller/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  Travel
+//
+//  Created by Sergio Herrera on 2/19/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemGray2
+    }
+}
