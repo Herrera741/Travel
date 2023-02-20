7094972b4184d2d79ea69b0dc9d1f8730bb45a68
Sergio <sherrera725@gmail.com>
2023-02-19 16:40:41 -0800

    add test populated collection view cells in OnboardingViewController

--- Travel/Controller/OnboardingViewController.swift
+++ Travel/Controller/OnboardingViewController.swift
@@ -9,16 +9,49 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        collectionView.backgroundColor = .systemBackground
+        collectionView.collectionViewLayout = layout
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
     }
     
     private func setupViews() {
-        view.backgroundColor = .systemMint
+        
     }
     
     @IBAction func getStartedBtnTapped(_ sender: UIButton) {
         performSegue(withIdentifier: K.Seque.showLoginSignupScreen, sender: nil)
     }
+}
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = indexPath.row % 2 == 0 ? .blue : .red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.frame.size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
 }
